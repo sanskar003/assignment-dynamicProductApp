f9ddc71a21add9365b3dc43bbeed0a74c755762d
sanskar <sanskar.003.02@gmail.com>
2025-11-14 22:53:44 +0530

    confi setting

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/assignment-Dynamic-product-filter---cart/',
+  base: '/assignment-dynamicProductApp/',
 });
